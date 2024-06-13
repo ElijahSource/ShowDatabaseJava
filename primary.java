25b76117e7e4d29c1707586ea6312e2f518714a5
Elijah-S-W <elijah.scott.white@gmail.com>
2024-06-13 14:16:23 -0700

    Added functionality to find seasons

--- primary.java
+++ primary.java
@@ -8,16 +8,60 @@ public class primary{
         String directoryInput = console.nextLine();
 
         File directory = new File(directoryInput); // replace with your directory
-        File[] files = directory.listFiles();
+        File[] Shows = directory.listFiles();
+        int i=0;
+        int y=0;
+        int z=0;
 
-        if (files != null) {
-            for (File file : files) {
+        if (Shows != null) {
+            for (File file : Shows) {
+                
                 if (file.isDirectory()) {
-                    System.out.println(file.getName());
+                    i++;
+                    System.out.println(i+" "+file.getName());
                 }
             }
-        } else {
+        } 
+        else {
             System.out.println(directory + " does not exist or is not a directory");
         }
+
+        int ShowNum = console.nextInt();
+
+        File[] Seasons = Shows[ShowNum].listFiles();
+        
+        if (Seasons != null) {
+            for (File file : Seasons) {
+                
+                if (file.isDirectory()) {
+                    y++;
+                    System.out.println(y+" "+file.getName());
+                }
+            }
+        } 
+        else {
+            System.out.println(directory + " does not exist or is not a directory");
+        }
+
+        int SeasonNum = console.nextInt();
+        File[] Episodes = Seasons[SeasonNum].listFiles();
+        
+        if (Episodes != null) {
+            for (File file : Episodes) {
+                
+                if (file=true) {
+                    z++;
+                    System.out.println(z+" "+file.getName());
+                }
+            }
+        } 
+        else {
+            System.out.println(directory + " does not exist or is not a directory");
+        }
+
+
+
+
+
     }
 }
