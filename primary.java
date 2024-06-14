1c7e02afc91a58a1ccd5b64f30969ad7f3a639c9
Elijah-S-W <elijah.scott.white@gmail.com>
2024-06-14 14:56:16 -0700

    Consolodated functions into one fucntion

--- primary.java
+++ primary.java
@@ -1,4 +1,4 @@
-import java.io.File; 
+import java.io.File;
 import java.util.Scanner;
 
 
@@ -8,18 +8,32 @@ public class primary{
         System.out.println("Enter your tv program directory, this program assumes your tv directory contains show directories, your show directories contain season directories, and your season directories contain episodes.");
         String directoryInput = console.nextLine();
 
-        File directory = new File(directoryInput); // replace with your directory
+        File directory = new File(directoryInput); 
         File[] Shows = directory.listFiles();
+
         int i=0;
-        int y=0;
+        
         int z=0;
 
-        if (Shows != null) {
-            for (File file : Shows) {
-                
-                if (file.isDirectory()) {
-                    i++;
-                    System.out.println(i+" "+file.getName());
+
+        /* 
+        if (Shows != null) {//if there are shows
+            for (File file : Shows) {//for every file in shows
+                File[] Seasons = Shows[i].listFiles();//array seasons = shows[i]
+                if (file.isDirectory()) {//if the file is a directory
+                    i++;//increment i
+                    if (Seasons != null) {//if there are seasons
+                        for (File file2 : Seasons) {//for every file in seasons
+                            int y=0;
+                            int count;
+                            if (file2.isDirectory()) {//if file is directory
+                                y++;//increment by 1
+                                File[] Episodes = Seasons[y].listFiles();
+                                System.out.println(String.valueOf(Seasons)+String.valueOf(Episodes.length));
+                            }
+                            
+                        }
+                    } 
                 }
             }
         } 
@@ -28,42 +42,40 @@ public class primary{
         }
 
         System.out.println("Select which show, by number");
-        int ShowNum = console.nextInt();
-        File[] Seasons = Shows[ShowNum].listFiles();
+        */
+  
         
-        if (Seasons != null) {
-            for (File file : Seasons) {
-                
-                if (file.isDirectory()) {
-                    y++;
-                    System.out.println(y+" "+file.getName());
-                }
-            }
-        } 
+/*
         else {
             System.out.println(directory + " does not exist or is not a directory");
         }
 
         System.out.println("Select which season, by number");
-        int SeasonNum = console.nextInt();
-        File[] Episodes = Seasons[SeasonNum].listFiles();
+
         
         if (Episodes != null){
             int count = Episodes.length;
             System.out.println(count);
 
         }
-
-        /* 
+ 
         if (Episodes != null) {
             for (File file : Episodes) {
                 z++;
                 System.out.println(z+" "+file.getName());
             }
-        } */
+        } 
         else {
             System.out.println(directory + " does not exist or is not a directory");
-        }
-        //These repeated functions can definitely be resolved to one function
+        }*/
+        //These repeated functions can definitely be resolved to one function 
     }
+
+    static File[] getChildFiles(String directory) {
+        File directoryInput = new File(directory); 
+        File[] Files = directoryInput.listFiles();
+        return Files;
+    }
+    
+    
 }
